d39866a3682b85fa9de1460b935eda2b01013b2a
Josh_corgi <whvkek@naver.com>
2024-08-25 14:49:50 +0900

    Init commit

---
+++ src/main/java/com/sparta/springupgradeschedule/SpringUpgradeScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springupgradeschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUpgradeScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUpgradeScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springupgradeschedule/SpringUpgradeScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springupgradeschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUpgradeScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
